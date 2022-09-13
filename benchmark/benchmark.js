3a5464087ab2e975004b48181668a39d0b614403
Jens Neuse <jens.neuse@gmx.de>
2022-09-13 12:42:14 +0200

    add workflow

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -2,8 +2,7 @@ import { check } from 'k6';
 import http from 'k6/http';
 
 export default function () {
-    //const url = 'http://localhost:4466';
-    const url = 'https://ujzriyxulz.fly.dev/';
+    const url = 'http://localhost:4466';
     //const payload = `{"query":"mutation CreatePost {createOnePost(data: {title: \\"myPost\\" author: {connect: {email: \\"jens@wundergraph.com\\"}}}){id title}}","operationName":"CreatePost"}`;
     const payload = `{"query":"query AllPosts {findManyPost(take: 2500){id title createdAt}}","operationName":"CreatePost"}`;
 
